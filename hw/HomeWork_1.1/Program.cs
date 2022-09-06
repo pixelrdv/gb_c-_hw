66aaf54c4794bca951fb1d4f785d43cb19f04628
Dmitriy Rogachev <i@rdv90.ru>
2022-09-06 16:32:31 +0300

    добавил домашнее задание

---
+++ hw/HomeWork_1.1/Program.cs
@@ -0,0 +1,20 @@
+﻿//===================================================================
+// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+//===================================================================
+
+
+
+Console.Write("Write a = ");
+string? a = Console.ReadLine();
+
+Console.Write("Write b = ");
+string? b = Console.ReadLine();
+
+if (a != null && b != null) {
+    int b_int = int.Parse(b);
+    int a_int = int.Parse(a);
+    Console.Write("max = ");
+    Console.WriteLine( Math.Max(a_int,b_int) );
+}
+
+
---
+++ hw/HomeWork_1.2/Program.cs
@@ -0,0 +1,28 @@
+﻿//===================================================================
+// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+//===================================================================
+
+
+Console.Write("Write a = ");
+string? a = Console.ReadLine();
+
+Console.Write("Write b = ");
+string? b = Console.ReadLine();
+
+Console.Write("Write c = ");
+string? c = Console.ReadLine();
+
+if (a != null && b != null && c != null) {
+    int b_int = int.Parse(b);
+    int a_int = int.Parse(a);
+    int c_int = int.Parse(c);
+    int[] ints = new int[3];
+    ints[0] = a_int;
+    ints[1] = b_int;
+    ints[2] = c_int;
+
+    Console.Write("max = ");
+    Console.WriteLine( ints.Max() );
+}
+
+
