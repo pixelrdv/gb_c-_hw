d0c91ba35206978a483d9c40c0ebc88a74c5b71e
Dmitriy Rogachev <i@rdv90.ru>
2022-09-09 17:55:40 +0300

    add second homework

---
+++ hw/HomeWork_2.1/Program.cs
@@ -0,0 +1,17 @@
+﻿//===================================================================
+// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+//===================================================================
+
+Console.Write("Write inputNumber = ");
+string inputNumber = Console.ReadLine();
+if (inputNumber != null && inputNumber.Length == 3)
+{
+    Console.WriteLine(inputNumber.ToCharArray()[1]);
+}
+else
+{
+    Console.WriteLine("Wrong format");
+}
---
+++ hw/HomeWork_2.3/Program.cs
@@ -0,0 +1,42 @@
+﻿//===================================================================
+// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+//===================================================================
+
+string inputNumber = "";
+int inputNumberInt = -1;
+
+void ReadData()
+{
+    Console.Write("Write inputNumber = ");
+    inputNumber = Console.ReadLine();
+}
+
+void IsHolyday()
+{
+    if (inputNumberInt > 5)
+    {
+        string answer = (inputNumberInt > 7) ? "Wrong format" : "да";
+        Console.WriteLine(answer);
+    }
+    else
+    {
+        Console.WriteLine("нет");
+    }
+}
+
+
+ReadData();
+
+if (inputNumber != null && inputNumber.Length == 1)
+{
+    Console.WriteLine(inputNumber);
+    inputNumberInt = int.Parse(inputNumber);
+    IsHolyday();
+}
+else
+{
+    Console.WriteLine("wrong format!");
+}
