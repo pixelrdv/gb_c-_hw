7719780f34bf80d0f4c60b8317b26ae3a1e1b2b6
Dmitriy Rogachev <i@rdv90.ru>
2022-09-15 18:34:28 +0300

    1*

--- hw/HomeWork_4.1/Program.cs
+++ hw/HomeWork_4.1/Program.cs
@@ -6,9 +6,11 @@
 // * Написать калькулятор с операциями +, -, /, * и возведение в степень
 //==================================================
 
-string ReadData()
+using System.Text.RegularExpressions;
+
+string ReadData(string stringToPrint)
 {
-    Console.Write("Write inputNumber = ");
+    Console.Write(stringToPrint);
     string inputNumber = Console.ReadLine();
     if (inputNumber != null)
     {
@@ -16,6 +18,8 @@ string ReadData()
     }
     else
     {
+        Console.WriteLine("Writed NULL string");
+        Environment.Exit(0);
         return "";
     }
 
@@ -36,12 +40,77 @@ long Pow(int A, int B)
     return res;
 }
 
-void PrintData(long res)
+
+//парсинг строки
+int[] getNumbers(string mathOperation)
 {
-    Console.WriteLine("A в степени В равно :" + res);
+    int[] numbers = new int[2];
+    Regex regex = new Regex(@"(-?\d+)");
+    MatchCollection reNumbers = regex.Matches(mathOperation);
+    if (reNumbers.Count == 2)
+    {
+        numbers[0]= int.Parse(reNumbers[0].Value);
+        numbers[1]= int.Parse(reNumbers[1].Value);
+    }
+    else
+    {
+        Console.WriteLine("Некорректная строка");
+        Environment.Exit(0);
+    }
+    return numbers;
 }
 
-string A = ReadData();
-string B = ReadData();
-long result = Pow( int.Parse(A) , int.Parse(B) );
-PrintData(result);
+string getOperator(string mathOperation)
+{
+    Regex regex = new Regex(@"(?<=\d|\s)(\+|\-|\/|\*|\^)");
+    MatchCollection reChar = regex.Matches(mathOperation);
+    if (reChar.Count > 0)
+    {
+        return  reChar[0].Value ;
+    }
+    else
+    {
+        Console.WriteLine("Некорректная строка");
+        Environment.Exit(0);
+    }
+    return "1";
+}
+
+void PrintData(string stringToPrint, double res)
+{
+    Console.WriteLine(stringToPrint + res);
+}
+
+Console.WriteLine("Справка.\nЕсли используете отрицательные числа, то перед вторым числом поставьте пробел.\n\nПример:\n-1* -7\n\n в противном случае можете писать любое количество пробелов между числами");
+string A = ReadData("\nВведите математическую операцию: ");
+
+int[] result = getNumbers(A);
+string mathOperator = getOperator(A);
+
+if (mathOperator == "+")
+{
+    PrintData("Сумма чисел равна : ", result[0] + result[1]);
+}
+
+if (mathOperator == "-")
+{
+    PrintData("Разность чисел равна : ", result[0] - result[1]);
+}
+
+if (mathOperator == "*")
+{
+    PrintData("Произведение чисел равно : ", result[0] * result[1]);
+}
+
+if (mathOperator == "/")
+{
+    PrintData("частное чисел равно : ", Convert.ToDouble(result[0]) / result[1])  ;
+}
+
+if (mathOperator == "^")
+{
+    PrintData("Возведение в степень равно : ", Pow(result[0] , result[1]) );
+}
+
+// long result = Pow(int.Parse(A), int.Parse(B));
+// PrintData(result);
