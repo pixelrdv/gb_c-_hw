3a357c958a68deb08c990c0a7971edb68ba4d1fd
Dmitriy Rogachev <i@rdv90.ru>
2022-09-23 18:25:02 +0300

    add hw6

---
+++ hw/HomeWork_6.1/Program.cs
@@ -0,0 +1,53 @@
+﻿//==================================================
+// №41 Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл
+// пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// -1, -7, 567, 89, 223-> 3
+// * Пользователь вводит число нажатий, затем программа следит за нажатиями и
+// выдает сколько чисел больше 0 было введено.
+//==================================================
+
+
+// using System;
+
+
+string ReadData(string stringToPrint)
+{
+    Console.Write(stringToPrint);
+    string inputNumber = Console.ReadLine();
+    if (inputNumber != null)
+    {
+        return inputNumber;
+    }
+    else
+    {
+        return "";
+    }
+
+}
+
+
+void CountPositive(int Mnum)
+{
+    if (Mnum <= 0)
+    {
+        Console.WriteLine("Некорректное число. Конец программы");
+        Environment.Exit(0);
+    }
+    
+    int count = 0;
+    while (Mnum > 0)
+    {
+        if (int.Parse(ReadData(" ")) > 0 )
+        {
+            count++;
+        }
+        Mnum--;
+    }
+    Console.WriteLine("Количество положительных чисел : " + count);
+}
+
+
+
+string M = ReadData("Введите количество цифр, которые будут введены : ");
+CountPositive(int.Parse(M));
---
+++ hw/HomeWork_6.2'/Program.cs
@@ -0,0 +1,85 @@
+﻿//==================================================
+// №43 Напишите программу, которая найдёт точку пересечения двух прямых, заданных
+// уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются
+// пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+// * Найдите площадь треугольника образованного пересечением 3 прямых
+//==================================================
+
+using System.Text.RegularExpressions;
+
+// main ======================
+int lineCount = 3;
+LineParamets[] lines = new LineParamets[lineCount];
+for (int i = 0; i < lineCount; i++)
+{
+    string inputString = ReadData(i);
+    if (inputString == "")
+    {
+        Console.WriteLine("Writed NULL string");
+        Environment.Exit(0);
+    }
+    ParseCoordinate(inputString, i);
+}
+
+CrossPoint(lines);
+
+
+// функция по считыванию входящего параметра
+string ReadData(int i)
+{
+    Console.WriteLine("enter the parameters of the line №" + (i + 1) + " separated by commas (k, b)");
+    string inputNumber = Console.ReadLine();
+    if (inputNumber != null)
+    {
+        return inputNumber;
+    }
+    else
+    {
+        return "";
+    }
+
+}
+
+//парсинг строки
+void ParseCoordinate(string param, int i)
+{
+
+    Regex regex = new Regex(@"(-?\d)");
+    MatchCollection paramArr = regex.Matches(param);
+    int j = 0;
+    LineParamets line = new LineParamets();
+    if (paramArr.Count > 0)
+    {
+        line.k = double.Parse(paramArr[0].Value);
+        line.b = double.Parse(paramArr[1].Value);
+        lines[i] = line;
+        line.Print();
+    }
+    else
+    {
+        Console.WriteLine("Некорректно введены координаты. Совпадений не найдено");
+        Environment.Exit(0);
+    }
+}
+
+//функция поиска точки пересечения
+void CrossPointPrint(LineParamets[] arr)
+{
+    double x = 0;
+    double y = 0;
+    
+    x = (arr[1].b - arr[0].b) / (arr[0].k - arr[1].k);
+    y = arr[0].k*x + arr[0].b;
+    Console.WriteLine(x + "  " + y);
+}
+
+class LineParamets
+{
+    public double k ;
+    public double b;
+    public void Print()
+    {
+        Console.WriteLine($"k : {k} , b : {b}");
+    }
+}
